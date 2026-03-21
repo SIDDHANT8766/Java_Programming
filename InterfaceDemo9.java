1724bf63f3d4fe43440e1fa3ab44c0b3edfd81f6
SIDDHANT8766 <gadkari.siddhant.vivek@gmail.com>
2026-03-22 01:12:04 +0530

    Add files via upload

---
+++ InterfaceDemo9.java
@@ -0,0 +1,39 @@
+interface A
+{
+    int no = 11;
+    void fun();
+    private void Display()             //Helper function
+    {
+        System.out.println("Inside private Displya ");
+    }
+    default void gun()
+    {
+        System.out.println("Inside gun");
+        Display();;
+    }
+   
+}
+
+
+class Demo implements A          
+{
+   public void fun()
+   {
+    System.out.println("Inside fun"+A.no);    
+   }
+
+}
+
+class InterfaceDemo9
+{
+    public static void main(String A[])
+    {
+        Demo dobj = new Demo();
+
+        dobj.fun();
+        dobj.gun();
+        //dobj.Display();              // Error
+
+         
+    }
+}
